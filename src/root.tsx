27f24526d0a969f5bb1ced15d3afd764e2cc9c21
Ash Simmonds <ashsimmonds@gmail.com>
2023-01-18 12:15:23 +1030

    purloin style stuff from old project

--- src/root.tsx
+++ src/root.tsx
@@ -20,6 +20,88 @@ export default function Root() {
         <Title>Solid Start Starter</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+
+
+
+
+
+
+
+        {/* <Link rel="preconnect" href="https://api.fonts.coollabs.io" />
+        <Link
+          href="https://api.fonts.coollabs.io/css2?family=Montserrat&family=Rajdhani:wght@600&family=Orbitron:wght@600&family=VT323&display=swap"
+          rel="stylesheet"
+        />
+
+        <Link rel="preconnect" href="https://fonts.googleapis.com" />
+        <Link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin="anonymous"
+        />
+        <Link
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600&display=swap"
+          rel="stylesheet"
+        /> */}
+
+
+
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          href="./fonts/montserrat-v25-latin-regular.woff2"
+          crossorigin="anonymous"
+        />
+
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          href="./fonts/orbitron-v25-latin-600.woff2"
+          crossorigin="anonymous"
+        />
+
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          href="./fonts/orbitron-v25-latin-regular.woff2"
+          crossorigin="anonymous"
+        />
+
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          href="./fonts/rajdhani-v15-latin-600.woff2"
+          crossorigin="anonymous"
+        />
+
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          href="./fonts/vt323-v17-latin-regular.woff2"
+          crossorigin="anonymous"
+        />
+
+
+
+
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://unpkg.com/augmented-ui@2/augmented-ui.min.css"
+        />
+
+
+
+
+
       </Head>
       <Body>
         <Suspense>
